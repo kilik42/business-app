131e420a742c22c8717e5256164c3594b4e19fc0
marvin evins <marvinevins@yahoo.com>
2024-06-05 13:33:47 -0500

    working on clerk implementation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import LoginScreen from "./../components/LoginScreen";
 export default function RootLayout() {
   useFonts({
     'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
@@ -14,13 +16,21 @@ export default function RootLayout() {
     'outfit-regular': require('../assets/fonts/Outfit-Regular.ttf'),
   })
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)"
-      options = {{
-          headerShown: false
+    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <SignedIn>
+        <Stack>
+          <Stack.Screen name="(tabs)"
+          options = {{
+              headerShown: false
 
-      }}
-      />
-    </Stack>
+          }}
+          />
+        </Stack>
+    </SignedIn>
+    <SignedOut>
+     <LoginScreen />
+    </SignedOut>
+
+    </ClerkProvider>
   );
 }
